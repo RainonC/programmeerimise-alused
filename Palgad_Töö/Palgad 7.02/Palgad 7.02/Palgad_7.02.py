4ff61005794ed8db72e787fcd02372f71e92a9a6
RainonC <118743359+RainonC@users.noreply.github.com>
2023-02-07 09:13:57 +0200

    Add files via upload

---
+++ Palgad_Töö/Palgad 7.02/Palgad 7.02/Palgad_7.02.py
@@ -0,0 +1,39 @@
+from mymodule import *
+palgad=[]
+nimed=[]
+
+palgad=loe_failist("palgade_fail.txt")
+print(palgad)
+nimed=loe_failist("nimede_fail.txt")
+print(nimed)
+
+
+
+while True:
+    v=int(input("0-loe failist \n1-andmete lisamine \n2-salvestamine failisse\n3-kustuta nimi failist\n4-maksimaalne palk\n5-minimaalne palk\n6-sorteerimine \nValik: "))
+    if v==0:
+        palgad=[]
+        nimed=[]
+        palgad=loe_failist("palgade_fail.txt")
+        nimed=loe_failist("nimede_fail.txt")
+        print(palgad)
+        print(nimed)
+    elif v==1:
+        palgad, nimed=elem_listisse(palgad,nimed)
+        print(palgad)
+        print(nimed)
+    elif v==2:
+        list_failisse(palgad,"palgade_fail.txt")
+        list_failisse(nimed,"nimede_fail.txt")
+    elif v==3:
+        palgad,nimed=kustutamine(input("Sisesta nimi: "), palgad,nimed)
+        print(palgad)
+        print(nimed)
+    elif v==4:
+        maksimaalne_palk(palgad,nimed)
+    elif v==5:
+        minimaalne_palk(palgad,nimed)
+    elif v==6:
+        palgad,nimed=sorteerimine(palgad,nimed,int(input("0 või 1 ")))
+        for i in range(len(palgad)):
+            print(f"{palgad[i]} - {nimed[i]}")
---
+++ Palgad_Töö/Palgad 7.02/Palgad 7.02/mymodule.py
@@ -0,0 +1,104 @@
+def loe_failist(file:str)->list:
+    """Loeme failist read ja lisame järjendisse 
+    :param str file: Faili nimetus
+    """
+    fail=open(file,'r',encoding="utf-8-sig")
+    mas=[]
+    for rida in fail:
+        if rida.isdigit():
+            mas.append(int(rida).strip())
+        else:
+            mas.append(rida.strip())
+    fail.close()
+    return mas
+
+def list_failisse(mas:list,file:str):
+    """Salvestamine loetelu failisse
+    :param str file: Faili nimetus
+    :param str mas: Loetelu
+    """
+    f=open(file,'w',encoding="utf-8-sig")
+    for item in mas:
+        f.write(item+"\n")
+    f.close()
+
+def elem_listisse(p:list,i:list):
+    """
+    """
+    n=int(input("Mitu inimesi lisame? "))
+    for j in range(n):
+            nimi=input("Nimi: ")
+            i.append(nimi)
+            p=input("Palk: ")
+            p.append(p)
+    print(p)
+    print(i)
+    return p,i
+
+def kustutamine(nimi:str,p:list,i:list):
+    """Kustutame failist nime ja palga
+    """
+    n=i.count(nimi)
+    pos=0
+    for j in range(n):
+        ind=i.index(nimi,pos)
+        pos=ind
+        i.remove(nimi)
+        p.pop(ind)
+    return p,i
+
+#def str_to_int(mas:list)->list:
+#    for m in mas:
+#        ind=mas.index(m)
+#        m=mas.pop(ind)
+#        m=int(m)
+#        mas.insert(m,ind)
+
+def maksimaalne_palk(p:list,i:list):
+    """Näitab maksimaalse palga listis
+    """   
+    p=list(map(int,p))
+    max_palk=max(p)
+    n=p.count(max_palk)
+    pos=0
+    print(f"Maksimaalne palk on {max_palk}\nInimeste nimed: ")
+    for j in range(n):
+        ind=p.index(max_palk)
+        nimi=i[ind]    
+        print(f"{nimi}")
+        pos=ind+1
+
+def minimaalne_palk(p:list,i:list):
+    """Näitab minimaalse palga listis
+    """
+    p=list(map(int,p))
+    min_palk=min(p)
+    ind=p.index(min_palk)
+    nimi=i[ind]
+    print(f"{nimi}-l on kõige väiksem palk {min_palk}")
+
+def sorteerimine(p:list, inimesed:list, a:int):
+    N=len(p)
+    p=list(map(int,p))
+    if a==0:
+        for i in range(0,N-1):
+            for j in range(i+1,N):
+                if p[i]<p[j]:
+                    abi=p[i]
+                    p[i]=p[j]
+                    p[j]=p[i] 
+                    abi=inimesed[i]
+                    inimesed[i]=inimesed[j]
+                    inimesed[j]=abi
+    else:
+        for i in range(0,N-1):
+            for j in range(i+1,N):
+                if p[i]>p[j]:
+                    abi=p[i]
+                    p[i]=p[j]
+                    p[j]=p[i] 
+                    abi=inimesed[i]
+                    inimesed[i]=inimesed[j]
+                    inimesed[j]=abi
+    return p,inimesed
+
